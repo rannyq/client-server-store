a1dcc1f8e46c0e6b2f26e750991fef50c7d01af8
rsue888 <49326613+rsue888@users.noreply.github.com>
2019-10-25 15:17:15 -0700

    with client

---
+++ client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	remote := flag.String("remote", "http://localhost:7777", "Server location")
+
+	flag.Parse()
+	start := time.Now()
+
+	resp, err := http.Get(*remote)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s said: %s after %s\n", *remote, bytes, time.Since(start))
+}
